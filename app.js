fa3ebe7dae0d9819bac9a6eeb3a969a34f4e0ecb
masontangnr <masontang0@gmail.com>
2025-11-19 13:25:41 -0600

    Adjust diamond size in QR code drawing and remove border radius from canvas

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ class QRCodeGenerator {
 
             case 'diamond':
                 // Draw diamond (rotated square) with spacing
-                const diamondSize = size * 0.7;
+                const diamondSize = size * 0.55;
                 this.ctx.save();
                 this.ctx.translate(x + size / 2, y + size / 2);
                 this.ctx.rotate(Math.PI / 4);
